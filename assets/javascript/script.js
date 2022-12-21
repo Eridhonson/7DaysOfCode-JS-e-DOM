466cc673b092238167f4d8b0cb4decec285084ef
Eridhonson Oliveira <eridhonson@outlook.com>
2022-12-21 11:35:49 -0500

    Mudança no inner utilizado para mostrar os resultados.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,5 +2,5 @@ let nameId = window.document.getElementById('name')
 let birthDate = window.document.getElementById('birth-date')
 
 function calc() {
-   document.getElementById('show').innerText = `${nameId.value} e ${birthDate.value}`
+   document.getElementById('show').innerHTML = `${nameId.value} e ${birthDate.value}`
 }
