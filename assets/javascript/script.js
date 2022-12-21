951013482ec4a91806870122316c53db2906d9a1
Eridhonson Oliveira <eridhonson@outlook.com>
2022-12-21 11:48:23 -0500

    Elemento focus aplicado.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,9 @@
-let nameId = window.document.getElementById('name')
+// Vincular uma função ao evento de submissão do formulário. A função vinculada deverá pegar o valor informado nos campos nome e data de nascimento e imprimi-lo.
+
+let namePlace = window.document.getElementById('name')
 let birthDate = window.document.getElementById('birth-date')
 
-function calc() {
-   document.getElementById('show').innerHTML = `${nameId.value} e ${birthDate.value}`
+function calc() { 
+   document.getElementById('show').innerHTML = `${namePlace.value} e ${birthDate.value}`
 }
+// namePlace.focus()
